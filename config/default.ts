0a9744b7d33470a775e2f49b55849f5199633115
GK <kevin.wang@atrustek.com>
2022-12-27 14:21:28 +0800

    Revoke access token key and add in .env, imporove accessing config data

--- config/default.ts
+++ config/default.ts
@@ -1,21 +1,26 @@
+import dotenv from 'dotenv';
+
+// Modify .env file in different environment
+dotenv.config();
+
 export default {
-    ip: '127.0.0.1',
-    port: 5000,
-    domain: 'http://localhost:4200',
+    ip: process.env.IP,
+    port: process.env.PORT,
+    domain: process.env.DOMAIN,
     db: {
-        host: 'localhost',
-        port: 3306,
-        database: 'account_system',
-        username: 'root',
-        password: 'password',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
     },
     smtp: {
-        host: 'smtp.mailtrap.io',
-        port: 2525,
-        user: 'ad36a2437f2039',
-        pass: '14a21f4b3c626b',
-        sender: 'gkgkdesign@gmail.com'
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+        sender: process.env.SMTP_SENDER
     },
-    accessTokenPrivateKey: 'LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlCT1FJQkFBSkJBSTZVc01xTklrQW5OSDB5Z2JOa0FpK0QxVlIvU2xmL1ZkR2QrYnhQY0lnVEd6WjlWYnZNCmF6enZZWEpScUQ5ZFdmejRCOWMvM0VkdmlLbkNJQWhtaVJNQ0F3RUFBUUpBRXQzYUJ2Tmw5TmM3T3RYVUU4akkKQVVHUW1QMisrS1h3MVN2eVA5akRLejRvLzJVMExMVzUzOWlzemswQzQ1K1grVTBiNEtuV25JczRWamFkMEcreApBUUloQU5seWFZVjdmaVJTNmk4K01rZ1BCQmNIWTFBR2MveGFPdURZSUh1VVpGRFZBaUVBcDl3NEU3eTFES2RZCjVYc25Yc0RPaHk4Tjg5M2FUVkZJMGtrcEZLaUNwa2NDSUZoZW9QZTRhbGxFd1J1V0FVVTdwUVBlNHNPQSs5NFEKcFdEL05LUkVlRWhKQWlCQ2xCeEI1OVUvd1ppRzNNUU8xeGFUazl4SzM3SFlmT2NQVVNsVUlrby92UUlnSG5Wagp0UnlRdWwrQk9lK3pXU3Vtb0ZYdWZQbVAzamc4VUdIb3VRSDRnTjA9Ci0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0t',
-    accessTokenPublicKey: 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZ3d0RRWUpLb1pJaHZjTkFRRUJCUUFEU3dBd1NBSkJBSTZVc01xTklrQW5OSDB5Z2JOa0FpK0QxVlIvU2xmLwpWZEdkK2J4UGNJZ1RHelo5VmJ2TWF6enZZWEpScUQ5ZFdmejRCOWMvM0VkdmlLbkNJQWhtaVJNQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ'
+    accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
+    accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY
 };
